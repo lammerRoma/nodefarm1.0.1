8d3bf33a1fb81e2f64e7955b15ac322b50736571
lammerRoma <astrolordfrecher@gmail.com>
2021-01-26 12:18:32 +0200

    added dependncies

--- test.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-let data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-
-const dataObj = JSON.parse(data);
-
-console.log(typeof(data));
-console.log(typeof(dataObj));
-
